e42ad78deb61876b2848388c7e9f22235b045f10
huaxia202020 <129567060+huaxia202020@users.noreply.github.com>
2024-03-30 17:39:50 +0800

    显示信息

--- LogLib.py
+++ LogLib.py
@@ -1,8 +1,10 @@
 import logging
 import time
-
+import os
 lt=time.localtime(time.time())
-logging.basicConfig(filename='{}-{}-{}-ASLog.txt'.format(lt.tm_year,lt.tm_mon,lt.tm_mday),
+if not os.path.exists('./logs'):
+    os.makedirs('./logs')
+logging.basicConfig(filename='./logs/{}-{}-{}-AS-Log.txt'.format(lt.tm_year,lt.tm_mon,lt.tm_mday),
                     format = '[%(asctime)s - %(name)s - %(funcName)s - %(levelname)s ] %(message)s',
                     level=logging.ERROR)
 logger = logging.getLogger('AS')
