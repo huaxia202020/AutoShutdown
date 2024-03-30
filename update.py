d112e8ac122e64b82464648a47b95f3416f12729
huaxia202020 <129567060+huaxia202020@users.noreply.github.com>
2024-03-30 16:59:02 +0800

    更新日志

--- update.py
+++ update.py
@@ -52,9 +52,9 @@ def is_old(old_time):
         return False
 
 
-if not os.path.exists('./last_update.txt'):
-    open('last_update.txt', 'w').write(str(time.localtime(time.time()))+"\n")
-old = is_old(os.path.getmtime('./last_update.txt'))
+if not os.path.exists('./UpdateLogs'):
+    open('UpdateLogs', 'w').write(str(time.localtime(time.time()))+"\n")
+old = is_old(os.path.getmtime('./UpdateLogs'))
 
 if old:
     print('检查到更新')
@@ -70,7 +70,7 @@ if old:
     copy_dir('./AutoShutdown-master', './')
     shutil.rmtree('./AutoShutdown-master')
     # 创建日期文件
-    open('last_update.txt', 'a').write(str(time.localtime(time.time()))+"\n")
+    open('UpdateLogs', 'a').write(str(time.localtime(time.time()))+"\n")
     print('更新完成')
 else:
     print('无更新')
