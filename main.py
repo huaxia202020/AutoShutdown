333076e784c14c407d1ceb767198a69e7675e809
huaxia202020 <129567060+huaxia202020@users.noreply.github.com>
2024-03-30 12:53:20 +0800

    update_test

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from decimal import Decimal
 from win11toast import toast
 import datetime
 
-
 def GetNowSec():
     return time.localtime(time.time()).tm_hour * 3600 + time.localtime(time.time()).tm_min * 60 + time.localtime(
         time.time()).tm_sec
