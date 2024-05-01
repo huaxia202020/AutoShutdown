42859969723cb6182975890155eddb9cfd9f166a
huaxia202020 <129567060+huaxia202020@users.noreply.github.com>
2024-05-01 11:39:18 +0800

    SundayTime

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ IsShow = False
 current_date = datetime.datetime.now().date()
 if current_date.weekday() == 6:
     toast('AutoShutdown', '今天为星期日，启用新的关机计划')
-    ShutdownTimes = [['17:40', 45], ['21:05', 5]]
+    ShutdownTimes = [['17:05', 45], ['21:30', 5]]
 if current_date.weekday() == 5:
     pass
 
